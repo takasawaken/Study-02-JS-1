8f9105f7409497ba67ca508eed9f41680bfed193
takasawaken <74123365+takasawaken@users.noreply.github.com>
2020-11-13 21:00:27 +0900

    Study 02 JS-1
    課題２回目 JS

---
+++ js/test.js
@@ -0,0 +1,88 @@
+// jsを記述する際はここに記載していく
+// console.log("はじめてのジャバスクリプト");
+// console.log(23+5);
+// console.log(2000-1800);
+// console.log("18+5");
+
+// var name = ("髙澤健");
+// console.log(name);
+
+// var point=20;
+
+// if(point >= 80){
+//     console.log("素晴らしい!おめでとう!")
+// }else{
+//     console.log("もっと頑張りましょう!")
+// }
+
+// var omikuji = Math.floor(Math.random() *5);
+// console.log(omikuji, "おみくじを作りました")
+// if(omikuji == 0){
+//     console.log("大吉です　おめでとう！")
+// }else if(omikuji == 1){
+//     console.log("中吉です　まずまず")
+// }else if(omikuji == 2){ 
+//     console.log("小吉です　ボチボチですな")
+// }else if(omikuji == 3){
+//     console.log("凶です　気をつけて")
+// }else if(omikuji == 4){
+//
+
+// $(function () {
+//     // この中に書いていく
+//     var omikuji = Math.floor(Math.random() *5);
+//     console.log(omikuji, "おみくじを作りました")
+//     if(omikuji == 0){
+//         console.log("大吉です　おめでとう！")
+//     }else if(omikuji == 1){
+//         console.log("中吉です　まずまず")
+//     }else if(omikuji == 2){ 
+//         console.log("小吉です　ボチボチですな")
+//     }else if(omikuji == 3){
+//         console.log("凶です　気をつけて")
+//     }else if(omikuji == 4){
+//         console.log("大凶です　・・・・")
+//     }
+
+//   });
+
+
+// $(".button").on("click", function(){
+
+// var omikuji = Math.floor(Math.random() *5);
+// console.log(omikuji, "おみくじを作りました")
+
+// if(omikuji == 0){
+//     console.log("大吉です　おめでとう！")
+//     $(".kekka").html("大吉です　おめでとう！")
+// }else if(omikuji == 1){
+//     console.log("中吉です　まずまず")
+//     $(".kekka").html("中吉です　まずまず")
+// }else if(omikuji == 2){ 
+//     console.log("小吉です　ボチボチですな")
+//     $(".kekka").html("小吉です　ボチボチですな")
+// }else if(omikuji == 3){
+//     console.log("凶です　気をつけて")
+//     $(".kekka").html("凶です　気をつけて")
+// }else if(omikuji == 4){
+//     console.log("大凶です　・・・・")
+//     $(".kekka").html("大凶です　・・・・")
+// }
+
+// jyanken == 0 : gu
+// jyanken == 1 : choki
+// jyanken == 2 : pa-
+
+//   });
+
+// // ceilは切り上げ、randomは数字(小数点以下)をランダムに表示
+// const num  = Math.ceil(Math.random()*5);
+//     alart(num);
+
+// // Math.floorは切り捨て関数
+// const num  = Math.floor(Math.random()*7+1);
+//     alart(num);
+
+
+
+
